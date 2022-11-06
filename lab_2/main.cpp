1fe2ef9156a022d29a189501a4339e334d2f39f6
Egor-Always-AFK <shokolokill@gmail.com>
2022-11-06 20:19:55 +0300

    Update main.cpp

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <conio.h>
-#include <iomanip>
-#include <sstream>
-#include <cstdint>
+#include<iostream>
 
 int main()
 {
@@ -13,49 +9,72 @@ int main()
 	//e = 0x2'192D'7B4E; 9'012'345'678;
 	//f = 0x2'5B67'B115; 10'123'456'789;
 	//result = (c*d + 23)/(a/2 - 4*d - 1);
-
-	uint32_t memo;
+	unsigned long long memo = 0;
 
 	__asm
 	{
 		pushad
 
-		//d*c (ebp:esp)
+		//d*c (ebx:ecx)+
 		mov edi, 0x1
-		mov esi, 0xD649'BAC0
+		mov esi, 0x7864CB
 		mov ecx, 0x2
-		mov ebx, 0x128E'0F87
+		mov ebx, 0x87D272
 		mov eax, esi
-		mul ebx
-		mov ebp, edx
-		mov esp, eax
+		mul ebx //esi ebx
+		push edx // h
+		push eax // l
 		mov eax, edi
-		mul ebx
-		add esp, eax
-		adc ebp, edx
+		mul ebx //edi ebx
+		push edx //h
+		push eax //l
 		mov eax, esi
-		mul ecx
-		add esp, eax
-		adc ebp, edx
+		mul ecx //esi ecx
+		push edx //h
+		push eax // l
 		mov eax, edi
-		mul ecx
-		add ebp, eax
+		mul ecx //edi ecx
+		//summ edx:eax
+		mov edx, eax
+		pop ebx //esi ecx l
+		pop ecx //esi ecx h
+		add eax, ebx
+		adc edx, ecx
+		pop ebx //edi ebx l
+		pop ecx //edi ebx h
+		add eax, ebx
+		adc edx, ecx
+		pop ebx //esi ebx l
+		pop ecx //esi ebx h
+		add eax, ebx
+		adc edx, ecx
+		mov ebx, edx
+		mov ecx, eax
 
-		//c*d + 23(ebp:esp)
+
+
+		//c*d + 23(ebx:ecx)+
 		mov eax, 0x17
-		add esp, eax
-		adc ebp, edx
+		mov edx, 0
+		add ecx, eax
+		adc ebx, edx
+		push ebx //h
+		push ecx //l
+		lea eax, tmp1
+		mov[eax], ebx
+		lea eax, tmp2
+		mov[eax], ecx
 
-		//a/2 (eax)
+		//a/2 (esi)+
 		mov edx, 0x1
-		mov eax, 0x527D'27F2
-		mov ecx, 0x2
-		div ecx
-		mov ebx, eax
+		mov eax, 0x56A735
+		mov edi, 2
+		div edi
+		mov esi, eax
 
-		//4*d (edi:esi)
+		//4*d (edi:esi)+
 		mov ecx, 0x4
-		mov eax, 0x128E'0F87
+		mov eax, 0x87D272
 		mov ebx, 0x2
 		mul ecx
 		mov esi, eax
@@ -64,24 +83,26 @@ int main()
 		mul ecx
 		add edi, eax
 
-		//a/2 - 4*d - 1 = (-1)(4*d-a/2 + 1)//4*d-a/2 + 1(edi:esi)
+		//4*d-a/2 + 1(edi:esi)+
 		mov edx, 0
 		sub esi, eax
 		sbb edi, edx
 		inc esi
 		sbb edi, edx
 
-		//(c*d + 23)/(4*d-a/2 + 1) // edi = 0 halyavi radi
-		mov edx, ebp
-		mov eax, esp
+		//(c*d + 23)/(4*d-a/2 + 1) 
+		mov eax, 0
+		mov edx, 0
+		pop eax //l
+		pop edx //h
 		div esi
 
-		//(c*d + 23)/(4*d-a/2 + 1)*(-1)
+		//(c*d + 23)/(4*d-a/2 + 1)*(-1)+
 		neg eax
 
 		lea ecx, memo
-		mov [ecx], eax
-		// mov dword ptr[result], eax
+		mov[ecx], eax
+
 		popad
 	}
 
